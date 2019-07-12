039f62a6f7b8bc8fcffa17d3dc3f108b219a3435
i321takeji <i.321.takeji@gmail.com>
2019-07-12 12:03:09 +0900

    Add Example2

---
+++ src/Example2/A.hs
@@ -0,0 +1,41 @@
+--A.hs
+--invoke: ghci -package ghc A.hs
+
+module Example2.A where
+
+import GHC
+import Outputable
+import GHC.LanguageExtensions.Type
+ 
+import GHC.Paths ( libdir )
+--GHC.Paths is available via cabal install ghc-paths
+ 
+import DynFlags
+targetFile = "B.hs"
+
+main :: IO ()
+main = do
+   res <- example
+   str <- runGhc (Just libdir) $ do
+      dflags <- getSessionDynFlags
+      return $ showSDoc dflags $ ppr res
+   putStrLn str
+ 
+example = 
+    defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+      runGhc (Just libdir) $ do
+        getSessionDynFlags >>= setSessionDynFlags
+        target <- guessTarget targetFile Nothing
+        setTargets [target]
+        load LoadAllTargets
+        modSum <- getModSummary $ mkModuleName "B"
+        p <- parseModule modSum
+        t <- typecheckModule p
+        d <- desugarModule t
+        l <- loadModule d
+        n <- getNamesInScope
+        c <- return $ coreModule d
+ 
+        g <- getModuleGraph
+        mapM showModule $ mgModSummaries g     
+        return $ (parsedSource d, "/n-----/n", typecheckedSource d)
