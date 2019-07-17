0da179c2b0cb3aa883f2f5386562b25f37af7115
i321takeji <i.321.takeji@gmail.com>
2019-07-17 18:04:18 +0900

    simplify

--- src/Example/Simple.hs
+++ src/Example/Simple.hs
@@ -1,20 +1,14 @@
 -- 参考: https://wiki.haskell.org/GHC/As_a_library
-{-# LANGUAGE CPP #-}
 module Example.Simple where
 
 import GHC
 import GHC.Paths ( libdir )
 import DynFlags
 
-main =
-#if __GLASGOW_HASKELL__ > 704
-    defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
-#else
-    defaultErrorHandler defaultLogAction $ do
-#endif
-      runGhc (Just libdir) $ do
-        dflags <- getSessionDynFlags
-        setSessionDynFlags dflags
-        target <- guessTarget "test_main.hs" Nothing
-        setTargets [target]
-        load LoadAllTargets
+main = defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+            runGhc (Just libdir) $ do
+            dflags <- getSessionDynFlags
+            setSessionDynFlags dflags
+            target <- guessTarget "test_main.hs" Nothing
+            setTargets [target]
+            load LoadAllTargets
